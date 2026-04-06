18f629868c6c16204fa71cba4874c801c423ac7a
kelleymarisa <kelleymarisa@ufl.edu>
2026-04-06 13:32:00 -0400

    deleted files (moved to restructure files)

--- IsoDAT2D/IsoDAT2D/__init__.py
+++
@@ -1,9 +0,0 @@
-# init file for the DAT2D package
-# Authors: Danielle N. Alverson
-# created: 2023-02-2
-# last modified: 2024-12-17
-# version: 1.0.0
-
-# import modules
-
-from .IsoDAT2D import *
